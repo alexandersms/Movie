cbce9cef204b625b196e913048d2da746d32a592
Alex Simisi <smsxander@gmail.com>
2019-09-11 18:20:53 +0200

    feature: création du bouton de recherche

--- src/components/search-bar.js
+++ src/components/search-bar.js
@@ -10,16 +10,28 @@ class SearchBar extends Component {
     this.setState({ searchText: e.target.value });
   }
 
+  handleOnClick(e) {
+    this.props.callback(this.state.searchText);
+  }
+
   render() {
     return (
       <div className="row">
-        <div className="col-md-8">
+        <div className="col-md-8 input-group">
           <input
             type="text"
             className="form-control input-lg"
             onChange={this.handleChange.bind(this)}
             placeholder={this.state.placeholder}
           />
+          <span className="input-group-btn">
+            <button
+              className="btn btn-secondary"
+              onClick={this.handleOnClick.bind(this)}
+            >
+              Rechercher
+            </button>
+          </span>
         </div>
       </div>
     );
--- src/containers/App.js
+++ src/containers/App.js
@@ -9,6 +9,7 @@ const API_END_POINT = "https://api.themoviedb.org/3/";
 const POPULAR_MOVIES_URL =
   "discover/movie?language=fr&sort_by=popularity.desc&include_adult=false&append_to_response=images";
 const API_KEY = "api_key=53259f4294e6568a8fcb1cdbdf90aa0c";
+const SEARCH_URL = "search/movie?language=fr&include_adult=false";
 
 class App extends Component {
   state = {
@@ -52,10 +53,47 @@ class App extends Component {
       );
   }
 
-  receiveCallBack(movie) {
+  onClickListItem(movie) {
     this.setState({ currentMovie: movie }, function() {
       this.applyVideoToCurrentMovie();
+      this.setRecommandation();
     });
+  }
+
+  setRecommandation() {
+    axios
+      .get(
+        `${API_END_POINT}movie/${this.state.currentMovie.id}/recommendations?${API_KEY}&language=fr`
+      )
+      .then(
+        function(response) {
+          this.setState({
+            movieList: response.data.results.slice(0, 5)
+          });
+        }.bind(this)
+      );
+  }
+
+  onClickSearch(searchText) {
+    if (searchText) {
+      axios
+        .get(`${API_END_POINT}${SEARCH_URL}&${API_KEY}&query=${searchText}`)
+        .then(
+          function(response) {
+            if (response.data && response.data.results[0]) {
+              if (response.data.results[0].id !== this.state.currentMovie.id) {
+                this.setState(
+                  { currentMovie: response.data.results[0] },
+                  () => {
+                    this.applyVideoToCurrentMovie();
+                    this.setRecommandation();
+                  }
+                );
+              }
+            }
+          }.bind(this)
+        );
+    }
   }
 
   render() {
@@ -64,7 +102,7 @@ class App extends Component {
         return (
           <VideoList
             movieList={this.state.movieList}
-            callback={this.receiveCallBack.bind(this)}
+            callback={this.onClickListItem.bind(this)}
           />
         );
       }
@@ -73,7 +111,7 @@ class App extends Component {
     return (
       <div>
         <div className="search_bar">
-          <SearchBar />
+          <SearchBar callback={this.onClickSearch.bind(this)} />
         </div>
         <div className="row">
           <div className="col-md-8">
